b0381444bc4cce23c6682f898d03743d17f6d505
LizbethFP <lizbeth.fp@gmail.com>
2018-02-24 18:51:51 -0500

    añadiendo precio, Q disponible, Q vendida, Q fracción a pagar y monto a pagar por fracción e imagen grande

--- public/js/data.js
+++ public/js/data.js
@@ -1,9 +1,9 @@
 // Categoría de celulares y smartphones
-const api = `https://api.mercadolibre.com/sites/MPE/search?q=smartphone&condition=new`;
+const api = 'https://api.mercadolibre.com/sites/MPE/search?q=smartphone&condition=new';
 
 fetch(api)
   .then(function(response) {
-    console.log(response);
+    // console.log(response);
     return response.json();
   }).then(function(data) {
     // Todas las marcas de celulares y smartphones
@@ -11,7 +11,32 @@ fetch(api)
     const phones = data['results'];
 
     phones.forEach((phone) => {
-      const phonesInThisCategory = phone['title'];
-      console.log(phonesInThisCategory);  
-    });
+      const phoneTitlesInThisCategory = phone['title'];
+      const phonesPrices = phone['price'];
+      const phonesAvailableQuantity = phone['available_quantity'];
+      const phonesSoldQuantity = phone['sold_quantity'];
+      const phonesInstallmentsquantity = phone['installments']['quantity'];
+      const phonesInstallmentsAmout = phone['installments']['amount'];
+      const targetID = phone['id'];
+      const fullInfoPhones = `https://api.mercadolibre.com/items/${targetID}`;
+
+
+      // const fullInfoPhones = phone['id'];
+      console.log('Nombre de cel ' + phoneTitlesInThisCategory);
+      console.log('Precio de cel en S/ ' + phonesPrices);
+      console.log('Cantidad disponible de cels ' + phonesAvailableQuantity);
+      console.log('Cantidad de cels vendidos ' + phonesSoldQuantity);   
+      console.log('Enlace a info completa ' + fullInfoPhones);
+      console.log('Cantidad de fracciones a pagar ' + phonesInstallmentsquantity);
+      console.log('Monto de fracción a pagar ' + phonesInstallmentsAmout);
+
+      fetch(fullInfoPhones)
+        .then(function(response) {
+          return response.json();         
+        })
+        .then(function(recurso) {
+          const phoneBigImg = recurso.pictures[0]['secure_url'];
+          // console.log(recurso.pictures[0]['secure_url']);
+        });
+    });      
   });
